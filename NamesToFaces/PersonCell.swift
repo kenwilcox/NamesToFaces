9f5cec090262a181540e9ec09f352ef09fcb14ba
Ken Wilcox <wilcoxk@gmail.com>
2015-11-27 22:29:50 -0700

    First stab at a UI

---
+++ NamesToFaces/PersonCell.swift
@@ -0,0 +1,14 @@
+//
+//  PersonCell.swift
+//  NamesToFaces
+//
+//  Created by Kenneth Wilcox on 11/27/15.
+//  Copyright © 2015 Kenneth Wilcox. All rights reserved.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+  @IBOutlet weak var imageView: UIImageView!
+  @IBOutlet weak var name: UILabel!
+}
