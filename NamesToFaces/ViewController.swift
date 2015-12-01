4d57ec2fdbfc8fb1bfa416326f5557e03a945122
Ken Wilcox <wilcoxk@gmail.com>
2015-12-01 00:01:13 -0700

    This should fix #12

--- NamesToFaces/ViewController.swift
+++ NamesToFaces/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
   
   @IBOutlet var collectionView: UICollectionView!
   
+  var people = [Person]()
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
@@ -33,12 +35,40 @@ class ViewController: UIViewController {
 
 extension ViewController:  UICollectionViewDataSource, UICollectionViewDelegate {
   func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-    return 10
+    return people.count
   }
   
   func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
     let cell = collectionView.dequeueReusableCellWithReuseIdentifier("Person", forIndexPath: indexPath) as! PersonCell
+    let person = people[indexPath.item]
+    
+    cell.name.text = person.name
+    
+    let path = getDocumentsDirectory().stringByAppendingPathComponent(person.image)
+    cell.imageView.image = UIImage(contentsOfFile: path)
+    
+    cell.imageView.layer.borderColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.3).CGColor
+    cell.imageView.layer.borderWidth = 2
+    cell.imageView.layer.cornerRadius = 3
+    cell.layer.cornerRadius = 7
+    
     return cell
+  }
+  
+  func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+    let person = people[indexPath.item]
+    
+    let ac = UIAlertController(title: "Rename person", message: person.name, preferredStyle: .Alert)
+    ac.addTextFieldWithConfigurationHandler(nil)
+    ac.addAction(UIAlertAction(title: "Cancel", style: .Cancel, handler: nil))
+    ac.addAction(UIAlertAction(title: "OK", style: .Default) { [unowned self, ac] _ in
+      let newName = ac.textFields![0]
+      person.name = newName.text!
+      
+      self.collectionView.reloadItemsAtIndexPaths([indexPath])
+      })
+    
+    presentViewController(ac, animated: true, completion: nil)
   }
 }
 
@@ -65,6 +95,10 @@ extension ViewController: UIImagePickerControllerDelegate, UINavigationControlle
     if let jpegData = UIImageJPEGRepresentation(newImage, 80) {
       jpegData.writeToFile(imagePath, atomically: true)
     }
+    
+    let person = Person(name: "Unknown", image: imageName)
+    people.append(person)
+    collectionView.reloadData()
     
     dismissViewControllerAnimated(true, completion: nil)
   }
